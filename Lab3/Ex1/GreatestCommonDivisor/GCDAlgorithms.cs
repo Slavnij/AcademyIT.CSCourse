ab1bb91bfdab96192b18df4f9d5ad8defaf31fc5
Evgeny Ipatov <101337644+Slavnij@users.noreply.github.com>
2022-06-17 05:49:39 -0700

    Add files via upload

---
+++ Lab3/Ex1/GreatestCommonDivisor/GCDAlgorithms.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace GreatestCommonDivisor
+{
+    static class GCDAlgorithms
+    {
+        // Add FindGCDEuclid method
+        public static int FindGCDEuclid(int a, int b)
+        {
+            int A = a, B = b;
+
+            if (a == 0) return b;
+
+            while (B > 0) 
+            {
+                if (A > B) A = A - B;
+                else B = B - A;
+            }
+
+            return A; 
+        }
+    }
+}
