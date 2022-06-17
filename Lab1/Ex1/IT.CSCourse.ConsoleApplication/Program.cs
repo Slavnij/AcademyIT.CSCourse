158613144a4f14601071f4c7abc48b052dcb4971
Evgeny Ipatov <101337644+Slavnij@users.noreply.github.com>
2022-06-17 05:43:48 -0700

    Add files via upload

---
+++ Lab1/Ex1/IT.CSCourse.ConsoleApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IT.CSCourse.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String InAndOutLine;
+
+            while ((InAndOutLine = Console.ReadLine()) != null) {
+
+                InAndOutLine = InAndOutLine.Replace(",", " y:");
+                InAndOutLine = "x:" + InAndOutLine;
+
+                Console.WriteLine(InAndOutLine);
+            }
+
+            //Console.ReadKey(true);
+        }
+    }
+}
